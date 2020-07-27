d0696fa6cded2e12660fbbb98d394dd39fe0b64d
Maynor Calix <calix2007@hotmail.com>
2020-07-26 21:16:16 -0600

    finalizando ToDo

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -1,32 +1,60 @@
-export class TodoList{
+import { Todo } from './todo.class';
 
+export class TodoList {
 
-    constructor(){
+    constructor() {
 
-        this.todos=[];
+        // this.todos = [];
+        this.cargarLocalStorage();
+
     }
 
-
-
-    nuevoTodo(todo){
-        this.todos.push(todo);
+    nuevoTodo( todo ) {
+        this.todos.push( todo );
+        this.guardarLocalStorage();
     }
 
-    eliminarTodo(id){
-       this.todos= this.todos.fill(todo=>todo.id!=id)
+    eliminarTodo( id ) {
+
+        this.todos = this.todos.filter( todo => todo.id != id )
+        this.guardarLocalStorage();
     }
 
-    marcarCompletado(id){
-        for(const todo of this.todos){
-            if(todo.id ==id)
-            {
-                todo.completado=!todo.completado;
+    marcarCompletado( id ) { 
+
+        for( const todo of this.todos ) {
+
+            if( todo.id == id ) {
+
+                todo.completado = !todo.completado;
+                this.guardarLocalStorage();
                 break;
             }
-        } 
+
+        }
+
+
     }
 
-    eliminarCompletados(){
-               this.todos= this.todos.fill(todo=>!todo.completadod)
+    eliminarCompletados() {
+        
+        this.todos = this.todos.filter( todo => !todo.completado )
+        this.guardarLocalStorage();
     }
+
+    guardarLocalStorage(){
+
+        localStorage.setItem('todo', JSON.stringify( this.todos ) );
+        
+    }
+
+    cargarLocalStorage(){
+
+        this.todos = ( localStorage.getItem('todo') )
+                        ? JSON.parse( localStorage.getItem('todo') )
+                        : [];
+        
+        this.todos = this.todos.map( Todo.fromJson );
+    }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import '../src/styles.css'
+import './styles.css';
 
-import {Todo,TodoList} from './classes'
-import {crearTodoHtml} from './js/componentes'
-
-export const todoList=new TodoList();
-
-const  tarea=new Todo('Aprender Javascript!');
-todoList.nuevoTodo(tarea);
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 
-console.log(todoList);
+export const todoList = new TodoList();
 
-crearTodoHtml(tarea);
+todoList.todos.forEach( crearTodoHtml );
+
+console.log( 'todos', todoList.todos );
